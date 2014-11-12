96a9b78e627c32c21046ed6a40ffbfc60586f308
Alex <office@cognitivebits.com>
2014-11-12 10:46:02 +0200

    added a test class

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Alex on 11/12/14.
+//  Copyright (c) 2014 Tedrasoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Alex on 11/12/14.
+//  Copyright (c) 2014 Tedrasoft. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
